24f7cb8e410f47f99b6e8c108d7f64e214dafdee
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-05-12 01:41:19 -0700

    adds some json parsing and testing

--- LaSonora-client/LaSonora-client.py
+++ LaSonora-client/LaSonora-client.py
@@ -2,11 +2,17 @@
 
 import pygame
 from pyomxplayer import OMXPlayer
+from json import loads, dumps
+from urllib2 import urlopen
 from time import time, sleep
 from os import path, listdir
 
+SERVER_ADDRESS = "http://astrovandalistas.cc/sonoratelematica"
+ENDPOINT_CLOCK = "reloj"
+ENDPOINT_FILEINFO = "dbinfo"
+
 MEDIA_CHANGE_FREQUENCY = 3
-LOCATION_FILTER = ["MEXICO", "RUSSIA"]
+LOCATION_FILTER = ["mexico", "russia"]
 DATE_FILTER = ["2010-01", "2012-02"]
 
 def _checkEvent():
@@ -15,10 +21,38 @@ def _checkEvent():
             (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
             raise KeyboardInterrupt
 
+def _makeFakeJSON():
+    ## DEBUG
+    brazil0 = ['bdjdjdj.wav', 'bfoo.txt', 'bhahaha.mp3']
+    mexico0 = ['mdjdjdj.wav', 'mfoo.txt', 'mhahaha.mp3']
+    x201001 = {}
+    x201001['brazil'] = brazil0
+    x201001['mexico'] = mexico0
+
+    brazil1 = ['b1djdjdj.wav', 'b1foo.txt', 'b1hahaha.mp3']
+    mexico1 = ['m1djdjdj.wav', 'm1foo.txt', 'm1hahaha.mp3']
+    russia1 = ['r1djdjdj.wav', 'r1foo.txt', 'r1hahaha.mp3']
+    x201202 = {}
+    x201202['brazil'] = brazil1
+    x201202['mexico'] = mexico1
+    x201202['russia'] = russia1
+
+    fakeData = {}
+    fakeData['2010-01'] = x201001
+    fakeData['2012-02'] = x201202
+
+    return dumps(fakeData)
+
 def setup():
     global omx
     global background, screen, font, mSurface, mSurfaceRect
     global lastMediaChangeTime, currentDateValue, currentDateFiles
+    global fileInfoData
+
+    ## TODO: rsync
+
+    #fileInfoData = loads(urlopen(SERVER_ADDRESS+"/"+ENDPOINT_FILEINFO).read())
+    fileInfoData = loads(_makeFakeJSON())
 
     omx = None
 
@@ -47,13 +81,14 @@ def setup():
     currentDateValue = "1900-00"
     currentDateFiles = []
 
-def _populateFileList():
+def _populateFileListFromLocalDir():
     global currentDateFiles, currentDateValue
+
+    currentDateFiles = []
 
     datePath = "./data/"+currentDateValue
     if(path.isdir(datePath)):
-        currentDateFiles = []
-        ## if no country filter, pick from all countries 
+        ## if no country filter, but date, pick from all countries
         if((not LOCATION_FILTER) and (currentDateValue in DATE_FILTER)):
             locations = [ l for l in listdir(datePath) if path.isdir(datePath+"/"+l)]
             for l in locations:
@@ -63,25 +98,47 @@ def _populateFileList():
             if(path.isdir(datePath+"/"+l)):
                 currentDateFiles.extend([datePath+"/"+l+"/"+f for f in listdir(datePath+"/"+l) if path.isfile(datePath+"/"+l+"/"+f)])
 
-        ## what we got?
+        ## print what we got
+        print "files: %s" % (currentDateFiles)
+
+def _populateFileListFromFileInfoData():
+    global currentDateFiles, currentDateValue
+    global fileInfoData
+
+    currentDateFiles = []
+
+    dataPath = "./data"
+    if(currentDateValue in fileInfoData):
+        ## if no country filter, but date, pick from all countries
+        if((not LOCATION_FILTER) and (currentDateValue in DATE_FILTER)):
+            for l in fileInfoData[currentDateValue]:
+                currentDateFiles.extend([dataPath+"/"+f for f in fileInfoData[currentDateValue][l]])
+        ## country filter
+        for l in LOCATION_FILTER:
+            if(l in fileInfoData[currentDateValue]):
+                currentDateFiles.extend([dataPath+"/"+f for f in fileInfoData[currentDateValue][l]])
+
+        ## print what we got
         print "files: %s" % (currentDateFiles)
 
 def loop():
     global omx
     global background, screen, font, mSurface, mSurfaceRect
     global lastMediaChangeTime, currentDateValue, currentDateFiles
+    global fileInfoData
 
     _checkEvent()
     background.fill((0,0,0))
 
     if(time() - lastMediaChangeTime > MEDIA_CHANGE_FREQUENCY):
         ## TODO: make request to server, get JSON
+        #inDateValue = loads(urlopen(SERVER_ADDRESS+"/"+ENDPOINT_CLOCK).read())['reloj']
         inDateValue = "2010-01"
 
         ## if a new date, populate list
         if(not inDateValue is currentDateValue):
             currentDateValue = inDateValue    
-            _populateFileList()
+            _populateFileListFromFileInfoData()
         lastMediaChangeTime = time()
 
         ## TODO: fade out ??
