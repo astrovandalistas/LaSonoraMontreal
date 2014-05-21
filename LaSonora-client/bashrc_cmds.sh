4ae6a279ea35a55d3fe5d3b9986d9c0a1e223780
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-05-21 11:30:49 -0400

    add file with bash cmds to autorun LaSonora

---
+++ LaSonora-client/bashrc_cmds.sh
@@ -0,0 +1,18 @@
+## S&S
+isrun=`ps -u pi | grep python | wc -l`
+if [ $isrun -lt 1 ] 
+then 
+    cd /home/pi/Dev/LaSonoraMontreal/LaSonora-client
+    export DISPLAY=":0.0"
+    startx &
+    rm -rf ./stop.sh
+    while [ ! -f ./stop.sh ]
+    do
+	python LaSonoraClient.py
+	killpid=$!
+	sleep 600
+	kill -9 $killpid
+    done
+    rm -rf ./stop.sh
+    sudo halt -n
+fi
